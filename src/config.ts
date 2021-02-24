8db3b0ed5dd86aa1c9f71fae41958e97d54e87a1
Romildo Junior <jose.romildo.junior@concrete.com.br>
2021-02-24 12:30:34 -0300

    feat: add config module

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,11 @@ import { Singleton } from "typescript-ioc";
 
 @Singleton
 export class Configuration {
-  public readonly port = 3000;
+  public readonly PORT = 3000;
+  public readonly REDIS = Object.freeze({
+    PORT: 6379,
+    HOST: 'localhost',
+    PASSWORD: 'sOmE_sEcUrE_pAsS',
+    SSL: false,
+  });
 }
